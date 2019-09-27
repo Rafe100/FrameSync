2362d2a4b2bd5ec2b15ef6f5283e4d4464b5077e
woshinikaikaige <936523016@qq.com>
2019-09-27 14:33:06 +0800

    Arrangement

---
+++ Client/Assets/Script/Net/NetClient.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System;
+
+public class NetClient : IDisposable
+{
+    public const Int32 MAX_PACKAGE_LENGTH = 1024 * 64 * 4;
+    public bool isConnected;
+    public ClientProtocol cProto {
+        get { return clientProto; }
+    }
+    protected string host;
+    protected int port;
+    protected Socket socket;
+    protected IPEndPoint endPoint;
+    protected Thread thread;
+    protected byte[] buffer;
+    protected Transporter transporter;
+    ClientProtocol clientProto;
+    Queue<System.Object> revQueue = new Queue<object>(29);
+
+   
+
+    public NetClient()
+    {
+        clientProto = new ClientProtocol();
+        buffer = new byte[MAX_PACKAGE_LENGTH];
+    }
+
+    public virtual void Start()
+    {
+        InitSocket();
+        Connect();
+    }
+
+    public void NetWorkMessageEnqueue(ReceiveData revData) {
+        if(revData == null) {
+            return;
+        }
+        this.revQueue.Enqueue(revData);
+    }
+
+    public ReceiveData NetWorkMessageDequeue() {
+        if (revQueue.Count <= 0) {
+            return null;
+        }
+        return this.revQueue.Dequeue() as ReceiveData;
+    }
+
+    public Socket GetSocket() {
+        return socket;
+    }
+
+
+    void Adress()
+    {
+        IPAddress address;
+        if (!IPAddress.TryParse(this.host, out address))
+        {
+            IPHostEntry hostEntry = Dns.GetHostEntry(this.host);
+            IPAddress[] ipAddrs = hostEntry.AddressList;
+            address = ipAddrs[0];
+        }
+        host = address.ToString();
+        endPoint = new IPEndPoint(address, port);
+    }
+
+    protected virtual void InitSocket() {
+        Adress();
+    }
+    
+    protected virtual void InitTransporter() {
+        this.transporter = new Transporter(this, this.socket);
+    }
+
+    protected virtual void Connect() {
+        InitTransporter();
+        this.socket.Connect(endPoint);
+        isConnected = true;
+    }
+
+    public virtual void Dispose() {
+        this.socket.Shutdown(SocketShutdown.Both);
+        this.socket.Close();
+        this.socket = null;
+    }
+
+}
---
+++ Client/Assets/Script/test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 a = new Vector2(0f, 0f);
+        Vector2 b = new Vector2(1.0f, 1.0f);
+
+        var v = Vector2.Lerp(a, b,0.3f);
+        FSFloat t = 1;
+        Debug.Log(v.ToString());
+        FSQuaternion r = new FSVector4(1,1,1,1);
+        //Quaternion
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
